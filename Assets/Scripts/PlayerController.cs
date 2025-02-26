612f5f827292d26569565b8fe365e3fd12fcc276
CaroliCarol <a59626@alunos.ipb.pt>
2025-02-26 17:23:25 +0000

    3.Test the default 2D Beginner movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-
     public InputAction MoveAction;
     // Start is called before the first frame update
     void Start()
     {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
         MoveAction.Enable();
     }
 
