c74a589a9f01eb8cf647e1f53ab3c913f71b83b6
CaroliCarol <a59626@alunos.ipb.pt>
2025-03-05 01:27:00 +0000

    5.Increase the player character health

--- Assets/Scripts/HealthCollect.cs
+++ Assets/Scripts/HealthCollect.cs
@@ -6,6 +6,12 @@ public class HealthCollect : MonoBehaviour
 {
         void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Object that entered the trigger: " + other);
+        PlayerController controller = other.GetComponent<PlayerController>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+        }
     }
 }
