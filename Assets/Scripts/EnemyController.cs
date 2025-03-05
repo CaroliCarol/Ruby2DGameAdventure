93d6cbee34a24c050d796c7efe88e8e62921ecd7
CaroliCarol <a59626@alunos.ipb.pt>
2025-03-05 17:17:34 +0000

    5.Challenge: Add a timer for patrol movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,14 +4,32 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    // Public Variables
     public float speed;
-   Rigidbody2D rigidbody2d;
-   public bool vertical;
+    public bool vertical;
+    public float changeTime = 3.0f;
+
+    // Private Variables
+    Rigidbody2D rigidbody2d;
+    float timer;
+    int direction = 1;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+    }
+
+    // Update is called every frame
+    void Update()
+    {
+        timer-= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
     }
 
     void FixedUpdate()
@@ -19,11 +37,11 @@ public class EnemyController : MonoBehaviour
         Vector2 position = rigidbody2d.position;
         if (vertical)
         {
-            position.y = position.y + speed * Time.deltaTime;
+            position.y = position.y + speed * direction * Time.deltaTime;
         }
         else
         {
-            position.x = position.x + speed * Time.deltaTime;
+            position.x = position.x + speed * direction * Time.deltaTime;
         }
         rigidbody2d.MovePosition(position);
     }
